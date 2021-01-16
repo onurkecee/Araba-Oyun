afddae773d81302f5d46f31bfe8fffc040a23ffc
Onur Keçe <onurkecee@gmail.com>
2021-01-16 20:11:00 +0300

    Initial commit

---
+++ Araba Oyun/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace Araba_Oyun
+{
+    partial class Araba_Oyun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Araba_Oyun));
+            this.cizgi1 = new System.Windows.Forms.PictureBox();
+            this.cizgi2 = new System.Windows.Forms.PictureBox();
+            this.cizgi4 = new System.Windows.Forms.PictureBox();
+            this.cizgi3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.araba = new System.Windows.Forms.PictureBox();
+            this.engel1 = new System.Windows.Forms.PictureBox();
+            this.engel3 = new System.Windows.Forms.PictureBox();
+            this.engel2 = new System.Windows.Forms.PictureBox();
+            this.altin2 = new System.Windows.Forms.PictureBox();
+            this.altin1 = new System.Windows.Forms.PictureBox();
+            this.altin3 = new System.Windows.Forms.PictureBox();
+            this.altin4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCoins = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTekrarOyna = new System.Windows.Forms.Button();
+            this.lblSonucCoins = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.lblHiz = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cizgi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cizgi2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cizgi4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cizgi3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altin2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altin3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altin4)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cizgi1
+            // 
+            this.cizgi1.BackColor = System.Drawing.Color.White;
+            this.cizgi1.Location = new System.Drawing.Point(181, 1);
+            this.cizgi1.Name = "cizgi1";
+            this.cizgi1.Size = new System.Drawing.Size(10, 85);
+            this.cizgi1.TabIndex = 0;
+            this.cizgi1.TabStop = false;
+            // 
+            // cizgi2
+            // 
+            this.cizgi2.BackColor = System.Drawing.Color.White;
+            this.cizgi2.Location = new System.Drawing.Point(181, 122);
+            this.cizgi2.Name = "cizgi2";
+            this.cizgi2.Size = new System.Drawing.Size(10, 85);
+            this.cizgi2.TabIndex = 1;
+            this.cizgi2.TabStop = false;
+            // 
+            // cizgi4
+            // 
+            this.cizgi4.BackColor = System.Drawing.Color.White;
+            this.cizgi4.Location = new System.Drawing.Point(181, 373);
+            this.cizgi4.Name = "cizgi4";
+            this.cizgi4.Size = new System.Drawing.Size(10, 85);
+            this.cizgi4.TabIndex = 2;
+            this.cizgi4.TabStop = false;
+            // 
+            // cizgi3
+            // 
+            this.cizgi3.BackColor = System.Drawing.Color.White;
+            this.cizgi3.Location = new System.Drawing.Point(181, 250);
+            this.cizgi3.Name = "cizgi3";
+            this.cizgi3.Size = new System.Drawing.Size(10, 85);
+            this.cizgi3.TabIndex = 3;
+            this.cizgi3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox5.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 461);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox6.Location = new System.Drawing.Point(374, 0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 461);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // araba
+            // 
+            this.araba.Image = ((System.Drawing.Image)(resources.GetObject("araba.Image")));
+            this.araba.Location = new System.Drawing.Point(112, 361);
+            this.araba.Name = "araba";
+            this.araba.Size = new System.Drawing.Size(39, 71);
+            this.araba.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.araba.TabIndex = 6;
+            this.araba.TabStop = false;
+            // 
+            // engel1
+            // 
+            this.engel1.Image = ((System.Drawing.Image)(resources.GetObject("engel1.Image")));
+            this.engel1.Location = new System.Drawing.Point(46, 78);
+            this.engel1.Name = "engel1";
+            this.engel1.Size = new System.Drawing.Size(39, 67);
+            this.engel1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.engel1.TabIndex = 7;
+            this.engel1.TabStop = false;
+            // 
+            // engel3
+            // 
+            this.engel3.Image = ((System.Drawing.Image)(resources.GetObject("engel3.Image")));
+            this.engel3.Location = new System.Drawing.Point(301, 168);
+            this.engel3.Name = "engel3";
+            this.engel3.Size = new System.Drawing.Size(39, 66);
+            this.engel3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.engel3.TabIndex = 8;
+            this.engel3.TabStop = false;
+            // 
+            // engel2
+            // 
+            this.engel2.Image = ((System.Drawing.Image)(resources.GetObject("engel2.Image")));
+            this.engel2.Location = new System.Drawing.Point(233, 16);
+            this.engel2.Name = "engel2";
+            this.engel2.Size = new System.Drawing.Size(39, 70);
+            this.engel2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.engel2.TabIndex = 9;
+            this.engel2.TabStop = false;
+            // 
+            // altin2
+            // 
+            this.altin2.Image = ((System.Drawing.Image)(resources.GetObject("altin2.Image")));
+            this.altin2.Location = new System.Drawing.Point(46, 198);
+            this.altin2.Name = "altin2";
+            this.altin2.Size = new System.Drawing.Size(25, 25);
+            this.altin2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.altin2.TabIndex = 12;
+            this.altin2.TabStop = false;
+            // 
+            // altin1
+            // 
+            this.altin1.Image = ((System.Drawing.Image)(resources.GetObject("altin1.Image")));
+            this.altin1.Location = new System.Drawing.Point(275, 281);
+            this.altin1.Name = "altin1";
+            this.altin1.Size = new System.Drawing.Size(25, 25);
+            this.altin1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.altin1.TabIndex = 13;
+            this.altin1.TabStop = false;
+            // 
+            // altin3
+            // 
+            this.altin3.Image = ((System.Drawing.Image)(resources.GetObject("altin3.Image")));
+            this.altin3.Location = new System.Drawing.Point(126, 27);
+            this.altin3.Name = "altin3";
+            this.altin3.Size = new System.Drawing.Size(25, 25);
+            this.altin3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.altin3.TabIndex = 14;
+            this.altin3.TabStop = false;
+            // 
+            // altin4
+            // 
+            this.altin4.Image = ((System.Drawing.Image)(resources.GetObject("altin4.Image")));
+            this.altin4.Location = new System.Drawing.Point(327, 61);
+            this.altin4.Name = "altin4";
+            this.altin4.Size = new System.Drawing.Size(25, 25);
+            this.altin4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.altin4.TabIndex = 15;
+            this.altin4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(11, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Coins:";
+            // 
+            // lblCoins
+            // 
+            this.lblCoins.AutoSize = true;
+            this.lblCoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCoins.Location = new System.Drawing.Point(64, 4);
+            this.lblCoins.Name = "lblCoins";
+            this.lblCoins.Size = new System.Drawing.Size(29, 20);
+            this.lblCoins.TabIndex = 17;
+            this.lblCoins.Text = "00";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SandyBrown;
+            this.panel1.Controls.Add(this.btnTekrarOyna);
+            this.panel1.Controls.Add(this.lblSonucCoins);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblGameOver);
+            this.panel1.Location = new System.Drawing.Point(60, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 212);
+            this.panel1.TabIndex = 18;
+            this.panel1.Visible = false;
+            // 
+            // btnTekrarOyna
+            // 
+            this.btnTekrarOyna.Location = new System.Drawing.Point(80, 128);
+            this.btnTekrarOyna.Name = "btnTekrarOyna";
+            this.btnTekrarOyna.Size = new System.Drawing.Size(114, 32);
+            this.btnTekrarOyna.TabIndex = 3;
+            this.btnTekrarOyna.Text = "Tekrar Oyna";
+            this.btnTekrarOyna.UseVisualStyleBackColor = true;
+            this.btnTekrarOyna.Click += new System.EventHandler(this.btnTekrarOyna_Click);
+            // 
+            // lblSonucCoins
+            // 
+            this.lblSonucCoins.AutoSize = true;
+            this.lblSonucCoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonucCoins.Location = new System.Drawing.Point(194, 77);
+            this.lblSonucCoins.Name = "lblSonucCoins";
+            this.lblSonucCoins.Size = new System.Drawing.Size(29, 20);
+            this.lblSonucCoins.TabIndex = 2;
+            this.lblSonucCoins.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(48, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Topladığınız Coin:";
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGameOver.ForeColor = System.Drawing.Color.Red;
+            this.lblGameOver.Location = new System.Drawing.Point(64, 22);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(148, 25);
+            this.lblGameOver.TabIndex = 0;
+            this.lblGameOver.Text = "GAME OVER";
+            // 
+            // lblHiz
+            // 
+            this.lblHiz.AutoSize = true;
+            this.lblHiz.ForeColor = System.Drawing.Color.Red;
+            this.lblHiz.Location = new System.Drawing.Point(318, 16);
+            this.lblHiz.Name = "lblHiz";
+            this.lblHiz.Size = new System.Drawing.Size(13, 13);
+            this.lblHiz.TabIndex = 19;
+            this.lblHiz.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(337, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "km/h";
+            // 
+            // Araba_Oyun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHiz);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblCoins);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.altin4);
+            this.Controls.Add(this.altin3);
+            this.Controls.Add(this.altin1);
+            this.Controls.Add(this.altin2);
+            this.Controls.Add(this.engel2);
+            this.Controls.Add(this.engel3);
+            this.Controls.Add(this.engel1);
+            this.Controls.Add(this.araba);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.cizgi3);
+            this.Controls.Add(this.cizgi4);
+            this.Controls.Add(this.cizgi2);
+            this.Controls.Add(this.cizgi1);
+            this.Name = "Araba_Oyun";
+            this.Text = "ONUR KEÇE | ARABA OYUNU";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.cizgi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cizgi2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cizgi4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cizgi3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altin2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altin3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altin4)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cizgi1;
+        private System.Windows.Forms.PictureBox cizgi2;
+        private System.Windows.Forms.PictureBox cizgi4;
+        private System.Windows.Forms.PictureBox cizgi3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox araba;
+        private System.Windows.Forms.PictureBox engel1;
+        private System.Windows.Forms.PictureBox engel3;
+        private System.Windows.Forms.PictureBox engel2;
+        private System.Windows.Forms.PictureBox altin2;
+        private System.Windows.Forms.PictureBox altin1;
+        private System.Windows.Forms.PictureBox altin3;
+        private System.Windows.Forms.PictureBox altin4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCoins;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTekrarOyna;
+        private System.Windows.Forms.Label lblSonucCoins;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.Label lblHiz;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Araba Oyun/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Araba_Oyun
+{
+    public partial class Araba_Oyun : Form
+    {
+        public Araba_Oyun()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SeritGecisi(oyunhizi);
+            Engeller(5);
+            Coins(oyunhizi);
+            AltinTopla();
+            GameOver();
+
+        }
+
+        int toplananaltin = 0;
+        Random r = new Random();
+        int x, y;
+        void Engeller(int hiz)
+        {
+            if (engel1.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                engel1.Location = new Point(x, 0);
+            }
+            else
+            {
+                engel1.Top += hiz;
+            }
+
+            if (engel2.Top >= 500)
+            {
+                x = r.Next(0, 400);
+                engel2.Location = new Point(x, 0);
+            }
+            else
+            {
+                engel2.Top += hiz;
+            }
+
+            if (engel3.Top >= 500)
+            {
+                x = r.Next(200, 350);
+                engel3.Location = new Point(x, 0);
+            }
+            else
+            {
+                engel3.Top += hiz;
+            }
+        }
+        void Coins(int hiz)
+        {
+            if (altin1.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                altin1.Location = new Point(x, 0);
+            }
+            else
+            {
+                altin1.Top += hiz;
+            }
+            if (altin2.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                altin2.Location = new Point(x, 0);
+            }
+            else
+            {
+                altin2.Top += hiz;
+            }
+            if (altin3.Top >= 500)
+            {
+                x = r.Next(50, 300);
+                altin3.Location = new Point(x, 0);
+            }
+            else
+            {
+                altin3.Top += hiz;
+            }
+            if (altin4.Top >= 500)
+            {
+                x = r.Next(0, 400);
+                altin4.Location = new Point(x, 0);
+            }
+            else
+            {
+                altin4.Top += hiz;
+            }
+        }
+        void GameOver()
+        {
+            if (araba.Bounds.IntersectsWith(engel1.Bounds))
+            {
+                timer1.Enabled = false;
+                panel1.Visible = true;
+            }
+            if (araba.Bounds.IntersectsWith(engel2.Bounds))
+            {
+                timer1.Enabled = false;
+                panel1.Visible = true;
+            }
+            if (araba.Bounds.IntersectsWith(engel3.Bounds))
+            {
+                timer1.Enabled = false;
+                panel1.Visible = true;
+            }
+            lblSonucCoins.Text = lblCoins.Text;
+
+        }
+        void SeritGecisi(int hiz)
+        {
+            if (cizgi1.Top >= 500)
+            {
+                cizgi1.Top = 0;
+            }
+            else
+            {
+                cizgi1.Top += hiz;
+            }
+
+            if (cizgi2.Top >= 500)
+            {
+                cizgi2.Top = 0;
+            }
+            else
+            {
+                cizgi2.Top += hiz;
+            }
+
+            if (cizgi3.Top >= 500)
+            {
+                cizgi3.Top = 0;
+            }
+            else
+            {
+                cizgi3.Top += hiz;
+            }
+
+            if (cizgi4.Top >= 500)
+            {
+                cizgi4.Top = 0;
+            }
+            else
+            {
+                cizgi4.Top += hiz;
+            }
+
+        }
+        void AltinTopla()
+        {
+            if (araba.Bounds.IntersectsWith(altin1.Bounds))
+            {
+                toplananaltin++;
+                lblCoins.Text = toplananaltin.ToString();
+                x = r.Next(0, 200);
+                altin1.Location = new Point(x, 0);
+            }
+            if (araba.Bounds.IntersectsWith(altin2.Bounds))
+            {
+                toplananaltin++;
+                lblCoins.Text = toplananaltin.ToString();
+                x = r.Next(0, 200);
+                altin2.Location = new Point(x, 0);
+            }
+            if (araba.Bounds.IntersectsWith(altin3.Bounds))
+            {
+                toplananaltin++;
+                lblCoins.Text = toplananaltin.ToString();
+                x = r.Next(0, 200);
+                altin3.Location = new Point(x, 0);
+            }
+            if (araba.Bounds.IntersectsWith(altin4.Bounds))
+            {
+                toplananaltin++;
+                lblCoins.Text = toplananaltin.ToString();
+                x = r.Next(0, 200);
+                altin4.Location = new Point(x, 0);
+            }
+        }
+        void HizArttir()
+        {
+            int hiz = 0;
+            hiz++;
+            lblHiz.Text = hiz.ToString();
+            
+        }
+        void HizAzalt()
+        {
+            int hiz = 0;
+            hiz--;
+            lblHiz.Text = hiz.ToString();
+        }
+
+        int oyunhizi = 0;
+
+        private void btnTekrarOyna_Click(object sender, EventArgs e)
+        {
+            Araba_Oyun araba = new Araba_Oyun();
+            araba.Show();
+            this.Hide();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (araba.Left > 0)
+                {
+                    araba.Left += -oyunhizi;
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (araba.Right < 380)
+                {
+                    araba.Left += oyunhizi;
+                }
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                if (oyunhizi < 21)
+                {
+                    oyunhizi++;
+                    HizArttir();
+                }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (oyunhizi > 0)
+                {
+                    oyunhizi--;
+                    HizAzalt();
+                }
+            }
+
+        }
+    }
+}
